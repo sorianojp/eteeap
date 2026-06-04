a63e2cfe6e67e67d4935bb431581c06eca0384a4
sorianojp <johnpaul.soriano.6197@gmail.com>
2026-06-04 09:49:12 +0800

    appservideprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+        if (config('app.env') === 'production') {
+        	URL::forceScheme('https');
+    	}
     }
 
     /**
